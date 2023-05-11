a7e2ddcb1b602c3764e4b94f28fcfb293383202b
erickpmass <erickpereiramassuco@gmail.com>
2023-05-10 22:36:55 -0300

    Affirmations Adapter

--- app/src/main/java/com/ds2/affirmations/MainActivity.kt
+++ app/src/main/java/com/ds2/affirmations/MainActivity.kt
@@ -2,14 +2,20 @@ package com.ds2.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ds2.affirmations.adapter.ItemAdapter
 import com.ds2.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-     val textView: TextView = findViewById(R.id.listItemCount)
-     textView.text = DataSource().loadAffirmations().size.toString()
+
+        val myDataset = DataSource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
